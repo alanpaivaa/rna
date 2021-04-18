3347e4ad034cf92aef44b58c785e600ce82a198c
Alan Paiva <jpaiva.alan@gmail.com>
2021-04-17 21:27:59 -0300

    Add small improvements on plotting

--- assignment2.py
+++ assignment2.py
@@ -9,6 +9,55 @@ from helpers.plot_helper import plot_decision_surface
 from helpers.realization import Realization
 
 
+def select_training_hyper_parameters(dataset, ratio=0.8, num_folds=5):
+    training_set, _ = train_test_split(dataset.load(), ratio, shuffle=True)
+
+    folds = list()
+    fold_size = int(len(training_set) / num_folds)
+    for i in range(num_folds):
+        fold = [training_set[i] for i in range(i*fold_size, (i+1) * fold_size)]
+        folds.append(fold)
+
+    learning_rate = 0.01
+    epochs = 50
+
+    for i in range(50):
+        # learning_rate *= 10
+        e = epochs * i
+
+        realizations = list()
+
+        for i in range(num_folds):
+            training_set = list()
+
+            # Flattening folds for training
+            for j in range(num_folds):
+                if j == i:
+                    continue
+                training_set += folds[j].copy()
+
+            model = Perceptron(epochs=e, learning_rate=learning_rate, early_stopping=False, verbose=False)
+            model.train(training_set)
+
+            val_set = folds[i]
+
+            classes = list()
+            predictions = list()
+
+            for row in val_set:
+                predictions.append(model.predict(row[:-1]))
+                classes.append(row[-1])
+
+            scores = Scores(classes, predictions)
+            realization = Realization(scores=scores)
+            realizations.append(realization)
+
+        accuracies = [r.scores.accuracy for r in realizations]
+        mean_accuracy = math_helper.mean(accuracies)
+        std_accuracy = math_helper.standard_deviation(accuracies)
+        print("Learning rate: {}, Epochs: {}, Accuracy: {:.2f}% ± {:.2f}%".format(learning_rate, e, mean_accuracy * 100, std_accuracy * 100))
+
+
 def evaluate(model, dataset, ratio=0.8, num_realizations=20, draw_decision_surface=False):
     max_scores = None
     errors = None
@@ -45,9 +94,6 @@ def evaluate(model, dataset, ratio=0.8, num_realizations=20, draw_decision_surfa
 
         realizations.append(realization)
 
-    print("Best confusion matrix")
-    max_scores.print_confusion_matrix()
-
     # print("Best accuracy: {:.2f}%".format(max_scores.accuracy * 100))
 
     accuracies = list(map(lambda r: r.scores.accuracy, realizations))
@@ -55,23 +101,32 @@ def evaluate(model, dataset, ratio=0.8, num_realizations=20, draw_decision_surfa
     std_accuracy = math_helper.standard_deviation(accuracies)
     print("Accuracy: {:.2f}% ± {:.2f}%".format(mean_accuracy * 100, std_accuracy * 100))
 
+    print("Best confusion matrix")
+    max_scores.print_confusion_matrix()
+
+    cia = 0  # Closest accuracy index
+    for i in range(1, len(accuracies)):
+        if abs(mean_accuracy - accuracies[i]) < abs(mean_accuracy - accuracies[cia]):
+            cia = i
+
+    print("Confusion matrix closest to the mean accuracy")
+    realizations[cia].scores.print_confusion_matrix()
+
     if not draw_decision_surface:
         plt.plot(errors)
-        plt.title("Soma dos erros por época")
         plt.xlabel("Épocas")
         plt.ylabel("Soma dos erros")
         plt.show()
 
     if draw_decision_surface:
-        cia = 0  # Closest accuracy index
-        for i in range(1, len(accuracies)):
-            if abs(mean_accuracy - accuracies[i]) < abs(mean_accuracy - accuracies[cia]):
-                cia = i
-
         # Set models with the "mean weights"
         model.weights = realizations[cia].weights
+        # print([[round(n, 4) for n in row] for row in realizations[cia].training_set])
+        # print([[round(n, 4) for n in row] for row in realizations[cia].test_set])
 
-        plot_decision_surface(model, realizations[cia].training_set + realizations[cia].test_set, offset=0.2)
+        plot_decision_surface(model, realizations[cia].training_set + realizations[cia].test_set,
+                              offset=0.2, xlabel="X", ylabel="Y",
+                              title="Superfície de Decisão", legend={0: '0', 1: '1'})
 
 
 # Iris dataset
@@ -92,8 +147,12 @@ dataset = Dataset('assignment2/datasets/iris.csv', encoding=iris_encodings[1])
 
 draw_decision_surface = "artificial" in dataset.filename
 
+learning_rate = 0.01
 ratio = 0.8
+epochs = 500
 
-model = Perceptron(epochs=500, learning_rate=0.01, early_stopping=False, verbose=False)
+# select_training_hyper_parameters(dataset)
+
+model = Perceptron(epochs=epochs, learning_rate=learning_rate, early_stopping=False, verbose=False)
 evaluate(model, dataset, ratio=ratio, num_realizations=20, draw_decision_surface=draw_decision_surface)
 
